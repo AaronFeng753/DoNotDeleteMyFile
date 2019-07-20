9b18b3cbc041ccbc3e50e828bd40be0d241cac1e
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2019-07-20 21:25:10 +0800

    中文文件名支持, 加入key的文件将统一放在一个文件夹内

--- DoNotDelMyFile/AddKey.py
+++ DoNotDelMyFile/AddKey.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import fileinput
 import re
 import random
@@ -6,10 +7,22 @@ import json
 import sys
 import os.path
 
-def AddTheKey(filename,TheKey):
-		with open(filename,'a') as f:
-			f.write(TheKey)
-		print("\nSuccess!\n")
+# def AddTheKey(filename,TheKey):
+	# with open(filename,'a') as f:
+		# f.write(TheKey)
+	# print("\nSuccess!\n")
+def AddTheKey(filename,TheKeym,filepath):
+	f = open (filepath, "rb+")
+	FileName=os.path.splitext(filename)[0]
+	FileExt=os.path.splitext(filename)[1]
+	current_dir = os.path.dirname(os.path.abspath(__file__))
+	KeyAddedPath = current_dir + '\\KeyAdded\\'
+	if os.path.exists(KeyAddedPath) == False:
+			os.mkdir(KeyAddedPath)
+	open(KeyAddedPath+FileName+FileExt, 'wb+').write(f.read())
+	open(KeyAddedPath+FileName+FileExt, 'a+').write(TheKey)
+	f.close()
+	print("\nSuccess!\n")
 		
 def GenerateTheKey():
 	randomint = str(random.randint(10000,9999999))
@@ -20,13 +33,13 @@ def SaveFileAndKey(filename,TheKey):
 	FileAndKey={}
 	try:
 		with open('FileAndKey.json','r') as f:
-			FileAndKey=json.load(f)
+			FileAndKey=json.load(f,ensure_ascii=False)
 	except FileNotFoundError:
 		with open('FileAndKey.json','w+') as f:
 			f.write('{}')
 	FileAndKey[filename]=TheKey
 	with open('FileAndKey.json','w+') as f:
-		json.dump(FileAndKey,f)
+		json.dump(FileAndKey,f,ensure_ascii=False)
 
 		
 
@@ -38,13 +51,13 @@ print('=  Add The Key        =')
 print('=======================')
 
 while True:
-	filename = ''
+	filepath = ''
 	try:
-		filename = str(sys.argv[1])
+		filepath = str(sys.argv[1])
 	except IndexError:
-		filename = input('file name: ')
+		filepath = input('file path&name: ')
 	TheKey = GenerateTheKey()
-	AddTheKey(filename,TheKey)
-	filename = str(os.path.basename(filename))
+	filename = str(os.path.basename(filepath))
+	AddTheKey(filename,TheKey,filepath)
 	SaveFileAndKey(filename,TheKey)
 	
