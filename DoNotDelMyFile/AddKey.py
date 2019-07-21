c601549148ae7701c90ce5347ec2c59a8d40fdb0
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2019-07-21 08:29:39 +0800

    Update AddKey.py

--- DoNotDelMyFile/AddKey.py
+++ DoNotDelMyFile/AddKey.py
@@ -7,45 +7,36 @@ import json
 import sys
 import os.path
 
-
-def AddTheKey(filename,TheKeym,filepath):
+def DelTheKey(filename,TheKey,filepath):
 	f = open (filepath, "rb+")
 	FileName=os.path.splitext(filename)[0]
 	FileExt=os.path.splitext(filename)[1]
 	current_dir = os.path.dirname(os.path.abspath(__file__))
-	KeyAddedPath = current_dir + '\\KeyAdded\\'
-	if os.path.exists(KeyAddedPath) == False:
-			os.mkdir(KeyAddedPath)
-	open(KeyAddedPath+FileName+FileExt, 'wb+').write(f.read())
-	open(KeyAddedPath+FileName+FileExt, 'a+').write(TheKey)
+	OriginalPath = current_dir + '\\Original\\'
+	if os.path.exists(OriginalPath) == False:
+		os.mkdir(OriginalPath)
+	open(OriginalPath+FileName+FileExt, 'wb+').write(re.sub(bytes(TheKey,encoding = 'utf8'),bytes(),f.read()))
 	f.close()
 	print("\nSuccess!\n")
-		
-def GenerateTheKey():
-	randomint = str(random.randint(10000,9999999))
-	randomdata = randomint
-	return '#DoNotDelMyFile//'+ randomdata +'#'
-
-def SaveFileAndKey(filename,TheKey):
+	
+def ReadFileAndKey(filename):
 	FileAndKey={}
 	try:
 		with open('FileAndKey.json','r') as f:
 			FileAndKey=json.load(f)
 	except FileNotFoundError:
-		with open('FileAndKey.json','w+') as f:
-			f.write('{}')
-	FileAndKey[filename]=TheKey
-	with open('FileAndKey.json','w+') as f:
-		json.dump(FileAndKey,f,ensure_ascii=False)
+		print('FileAndKey.json is missing!!')
+	
+	for key,value in FileAndKey.items():
+		if key == filename:
+			return str(value)
 
-		
-
-print('=======================')
-print('=  DoNotDeleteMyFile') 
-print('=  2019.7.21') 
-print('=') 
-print('=  向文件末尾添加Key') 
-print('=======================')
+print('=============================')
+print('=  DoNotDeleteMyFile')               
+print('=  2019.7.21')                       
+print('=')                                  
+print('=  删除文件内的Key & 假解密')          
+print('=============================')
 print('输入文件路径+名称(例:C:\\Users\\123\\Desktop\\images.jpg),然后按Enter')
 print('tip:你可以直接把文件拖拽到窗口内,文件路径和名称会自动填写\n')
 
@@ -56,8 +47,8 @@ while True:
 	except IndexError:
 		filepath = input('file path&name: ')
 		filepath=filepath.strip('"')
-	TheKey = GenerateTheKey()
 	filename = str(os.path.basename(filepath))
-	AddTheKey(filename,TheKey,filepath)
-	SaveFileAndKey(filename,TheKey)
+	TheKey = ReadFileAndKey(filename)
+	DelTheKey(filename,TheKey,filepath)
+
 	
